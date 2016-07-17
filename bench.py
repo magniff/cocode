fb7fafe38c98a23fbfa5fb970f29b68dfbd0dc72
magniff <tinysnippets@gmail.com>
2016-07-17 12:43:43 -0400

    Add simple bench.

---
+++ bench.py
@@ -0,0 +1,74 @@
+import timeit
+from functools import reduce
+from operator import mul
+from cocode import *
+
+
+factorial_asm = CodeObjectProxy(
+    Constant(1),
+    Bind("result"),
+    Label(Variable("value"), "start"),
+    Constant(1),
+    Compare("=="),
+    JumpTrue("all_done"),
+    Variable("value"),
+    Variable("result"),
+    Mult(),
+    Bind("result"),
+    Variable("value"),
+    Constant(-1),
+    Add(),
+    Bind("value"),
+    Jump("start"),
+    Label(Variable("result"), "all_done"),
+    Return(),
+)
+
+
+fac_asm_code = factorial_asm.assemble()
+
+
+def factorial_asm(value):
+    return eval(fac_asm_code, {}, {"value": value})
+
+
+def factorial_recursive(value):
+    return 1 if value == 1 else factorial_recursive(value - 1) * value
+
+
+def factorial_forloop(value):
+    result = 1
+    for _ in range(1, value + 1):
+        result *= 1
+    return result
+
+
+def factorial_whileloop(value):
+    result = 1
+    while value > 1:
+        result *= value
+        value -= 1
+    return result
+
+
+factorial_reduce = lambda value: reduce(mul, range(1, value + 1), 1)
+
+
+print("Benching factorial asm...")
+print(timeit.timeit(lambda: factorial_asm(900), number=10000))
+
+
+print("Benching factorial recursive...")
+print(timeit.timeit(lambda: factorial_recursive(900), number=10000))
+
+
+print("Benching factorial for loop...")
+print(timeit.timeit(lambda: factorial_forloop(900), number=10000))
+
+
+print("Benching factorial while loop...")
+print(timeit.timeit(lambda: factorial_whileloop(900), number=10000))
+
+
+print("Benching factorial reduce...")
+print(timeit.timeit(lambda: factorial_reduce(900), number=10000))
